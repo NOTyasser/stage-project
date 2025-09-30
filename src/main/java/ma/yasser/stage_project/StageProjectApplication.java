0a4e16bb61b9c458c558828ac571414b1de12b36
Notyasser <showhad55@gmail.com>
2025-10-01 00:58:12 +0200

    initial commit

---
+++ src/main/java/ma/yasser/stage_project/StageProjectApplication.java
@@ -0,0 +1,13 @@
+package ma.yasser.stage_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/yasser/stage_project/StageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.yasser.stage_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
