eae63914eddcf2f4b889f7179ea29eac17f5765f
Notyasser <showhad55@gmail.com>
2025-10-09 02:53:58 +0200

    changes

--- src/main/java/ma/yasser/stage_project/model/User.java
+++ src/main/java/ma/yasser/stage_project/model/User.java
@@ -1,30 +1,5 @@
-package ma.yasser.stageproject.model;
+package ma.yasser.stage_project.model;
 
-public class User {
-    private String email;
-    private String password;
-
-    // 🔹 constructeur vide
-    public User() {}
-
-    // 🔹 constructeur avec paramètres
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    // 🔹 getters et setters
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public class user {
+    
 }
